40925b9b34befd461d5f61af20923f82b77ae9f8
Douglas Thomaz <douglasthmz@hotmail.com>
2020-01-09 02:17:12 -0300

    novas feats visuais e delete de itens da tabela
    validações de form implementadas

--- src/components/formulario/Form.js
+++ src/components/formulario/Form.js
@@ -3,43 +3,126 @@ import Form from 'react-bootstrap/Form'
 import { Col, Button } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import './Form.css'
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 class Formulario extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+
+        const schemaValidacao = Yup.object().shape({
+            entrada: Yup.string()
+                .min(0, "*Não pode haver numeros negativos")
+                .max(6, "*O mercado hoje não subiu isso tudo, coloque valores decentes")
+                .required("*Valor de entrada é obrigatório"),
+            saida: Yup.string()
+                .min(0, "*Não pode haver numeros negativos")
+                .max(6, "*O mercado hoje não subiu isso tudo, coloque valores decentes")
+                .required("*Valor de saída é obrigatório"),
+            contratos: Yup.string()
+                .min(1, "*Pelo menos 1 contrato né?")
+                .max(4, "*Você não é o banco Itau!")
+                .required("*Coloque pelo menos 1 contrato"),
+            tipo: Yup.string()
+
+        });
         return (
-            
-                <Container>
-                    <div id="formulario">
-                    <Form>
-                        <Form.Row>
-                        <Form.Group as={Col} xs={6} md={3}controlId="entrada">
-                            <Form.Label>Entrada</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
+            <Container>
+                <div id="formulario">
+                    <Formik
+                        initialValues={{ entrada: "", saida: "", contratos: "1", tipo: "" }}
+                        validationSchema={schemaValidacao}
+                        onSubmit={(values, { setSubmitting, resetForm }) => {
+                            // When button submits form and form is in the process of submitting, submit button is disabled
+                            setSubmitting(true);
 
-                        <Form.Group as={Col} xs={6} md={3} controlId="saida">
-                            <Form.Label>Saida</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
-                        <Form.Group as={Col} xs={6} md={3} controlId="contratos">
-                            <Form.Label>Contratos</Form.Label>
-                            <Form.Control />
-                        </Form.Group>
+                            // Resets form after submission is complete
+                            resetForm();
 
-                        <Form.Group as={Col} xs={6} md={3} controlId="tipoOperacao">
-                            <Form.Label>Tipo de operação</Form.Label>
-                            <Form.Control as="select">
-                                <option>Compra</option>
-                                <option>Venda</option>
-                            </Form.Control>
-                        </Form.Group>
-                        </Form.Row>
-                        <Button variant="outline-light" type="submit" xs={12} md={1}>Enviar Trade</Button>  
-                        
-                    </Form>
-                    </div>
-                </Container>
-            
+                            // Sets setSubmitting to false after form is reset
+                            setSubmitting(false);
+                        }}>
+
+                        {/* Callback function containing Formik state and helpers that handle common form actions */}
+                        {({ values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting }) => (
+
+                                <form onSubmit={handleSubmit}>
+                                    <Form>
+                                        {console.log(errors.entrada)}
+                                        <Form.Row>
+                                            <Form.Group as={Col} xs={6} md={3} controlId="entrada">
+                                                <Form.Label>Entrada</Form.Label>
+                                                <Form.Control
+                                                    type="string"
+                                                    /* This name property is used to access the value of the form element via values.nameOfElement */
+                                                    name="entrada"
+                                                    placeholder="Entrada"
+                                                    /* Set onChange to handleChange */
+                                                    onChange={handleChange}
+                                                    /* Set onBlur to handleBlur */
+                                                    onBlur={handleBlur}
+                                                    /* Store the value of this input in values.name, make sure this is named the same as the name property on the form element */
+                                                    value={values.entrada}
+                                                    className={touched.entrada && errors.entrada ? "error" : null} />
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} xs={6} md={3} controlId="saida">
+                                                <Form.Label>Saida</Form.Label>
+                                                <Form.Control
+                                                    type="string"
+                                                    name="saida"
+                                                    placeholder="Saída"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.saida}
+                                                    className={touched.saida && errors.saida ? "error" : null} />
+                                            </Form.Group>
+                                            <Form.Group as={Col} xs={6} md={3} controlId="contratos">
+                                                <Form.Label>Contratos</Form.Label>
+                                                <Form.Control
+                                                    type="string"
+                                                    name="contratos"
+                                                    placeholder="Contratos"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.contratos}
+                                                    className={touched.contratos && errors.contratos ? "error" : null} />
+                                            </Form.Group>
+
+                                            <Form.Group as={Col} xs={6} md={3} controlId="tipoOperacao">
+                                                <Form.Label><span>Tipo</span></Form.Label>
+                                                <Form.Control
+                                                    as="select"
+                                                    name="tipo"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.tipo}>
+                                                    <option value="compra">Compra</option>
+                                                    <option value="venda">Venda</option>
+                                                </Form.Control>
+                                            </Form.Group>
+                                        </Form.Row>
+                                        <Button variant="outline-light" type="submit" disabled={isSubmitting} xs={12} md={1}>Enviar Trade</Button>
+
+                                    </Form>
+                                </form>
+
+                            )}
+                    </Formik>
+
+                </div>
+            </Container>
+
 
         );
     }
--- src/components/tabela/Tabela.js
+++ src/components/tabela/Tabela.js
@@ -1,61 +1,90 @@
 import React, { Component } from 'react';
 import api from '../services/Api';
-import { Container, Table } from 'react-bootstrap';
-import { parseISO, format} from 'date-fns';
+import { Container, Table, Button } from 'react-bootstrap';
+import { parseISO, format } from 'date-fns';
+import { FaHotjar, FaSortNumericDown, FaSortNumericUp } from "react-icons/fa";
 
-  import './tabela.css';
+import './tabela.css';
 
 export default class Tabela extends Component {
-    state={
+    constructor(props){
+        super(props);
+        this.removeTradeporId= this.removeTradeporId.bind(this);
+    }
+    
+    state = {
         trades: []
     }
     componentDidMount() {
         this.carregaTrades();
     }
 
-    carregaTrades = async() => {
+    carregaTrades = async () => {
         const response = await api.get('/trades');
-        this.setState({trades: response.data})
+        this.setState({ trades: response.data })
+
+    }
+
+    async removeTradeporId(id){
+        const url = '/trades/' + id;
+        const res = await api.delete(url);
+        console.log(res.status, id);
+        this.carregaTrades();
 
     }
 
     render() {
         return (
             <Container>
-            <Table striped responsive hover variant="dark">
-                 <thead>
-                     <tr>
-                        <th>Tipo</th>
-                        <th>Entrada</th>
-                        <th>Saida</th>
-                        <th>Pontos</th>
-                        <th>Contratos</th>
-                        <th>Saldo</th>
-                        <th>Data</th>
-                     </tr>
-                 </thead>
-                 
-                 <tbody>
-                {this.state.trades.map(trade=> {
-                    const datajs = parseISO(trade.data);
-                    const dataFormatada=format(
-                        datajs,
-                        "dd'/'MM'/'yyyy"
-                    );
+                <Table striped responsive hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>Tipo</th>
+                            <th>Entrada</th>
+                            <th>Saida</th>
+                            <th>Pontos</th>
+                            <th>Contratos</th>
+                            <th>Saldo</th>
+                            <th>Data</th>
+                            <th></th>
 
-                    
-                    return (
-                    <tr key={trade._id}>
-                        <td>{trade.tipo}</td>
-                        <td>{trade.entrada}</td>
-                        <td>{trade.saida}</td>
-                        <td>{trade.pontos}</td>
-                        <td>{trade.contratos}</td>
-                        <td>{trade.saldo}</td>
-                        <td>{dataFormatada}</td>
-                    </tr> 
-                )})}
-                </tbody>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.state.trades.map(trade => {
+                            const datajs = parseISO(trade.data);
+                            const dataFormatada = format(
+                                datajs,
+                                "dd'/'MM'/'yyyy"
+                            );
+                            let idSaldo = "positivo";
+                            let saldo = parseFloat(trade.saldo);
+                            if (saldo <= 0) {
+                                idSaldo = "negativo";
+                            }
+
+                            let Icon = FaSortNumericUp;
+                            let idCss = "compra"
+                            if (trade.tipo === 'venda') {
+                                Icon = FaSortNumericDown;
+                                idCss = "venda";
+                            }
+
+                            return (
+                                <tr key={trade._id}>
+                                    <td><Icon id={idCss} /></td>
+                                    <td>{trade.entrada}</td>
+                                    <td>{trade.saida}</td>
+                                    <td>{trade.pontos}</td>
+                                    <td>{trade.contratos}</td>
+                                    <td id={idSaldo}>{trade.saldo}</td>
+                                    <td>{dataFormatada}</td>
+                                    <td><Button onClick={ () => this.removeTradeporId(trade._id)} variant="outline-light"><FaHotjar /></Button></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </Table>
             </Container>
         );
